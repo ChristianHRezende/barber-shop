30473201669b3f26449c65bd411acb298d375a58
Christian H Rezende <christian_srs_@hotmail.com>
2021-03-20 16:57:58 -0300

    refactor: remove unused imports home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,9 @@
 import React, { ReactNode } from 'react';
 import Footer from '../../components/layout/Footer';
 import Header from '../../components/layout/Header';
-import barberIcon from '../../assets/img/logo.png';
 import Main from '../../components/layout/Main';
-import Banner from './components/Banner';
 import AboutUs from './components/AboutUs';
+import Banner from './components/Banner';
 
 interface HomePageProps {
   children?: ReactNode;
