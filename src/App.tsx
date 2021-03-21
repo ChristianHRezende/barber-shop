9b8400acdb991ce8df2352c64df5c02e8358fdfc
Christian H Rezende <christian_srs_@hotmail.com>
2021-03-21 12:11:15 -0300

    feature: connect to store and get dynamic data

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import React, { ReactNode } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import HomePage from './pages/Home';
 import ProcutsPage from './pages/Products';
+import { Provider } from 'react-redux';
+
+import { store } from './redux/store';
 
 interface AppProps {
   children?: ReactNode;
@@ -9,16 +12,18 @@ interface AppProps {
 
 const App: React.FC<AppProps> = () => {
   return (
-    <Router>
-      <Switch>
-        <Route path="/products">
-          <ProcutsPage />
-        </Route>
-        <Route path="/">
-          <HomePage />
-        </Route>
-      </Switch>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Switch>
+          <Route path="/products">
+            <ProcutsPage />
+          </Route>
+          <Route path="/">
+            <HomePage />
+          </Route>
+        </Switch>
+      </Router>
+    </Provider>
   );
 };
 
